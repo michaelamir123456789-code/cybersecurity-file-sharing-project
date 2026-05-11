e9bb6b9b445b0bade2d591a202f3d23ed927c9e9
MichaelAmir0 <michaelamir123456789@gmail.com>
2026-05-11 23:05:17 +0300

    Update and rename Login.js to login.js

--- login.js
+++ login.js
@@ -13,6 +13,7 @@
         }
 
         // Add loading animation on form submit
+        // loginBtn: el button el bey submit el form (Sign In)
         document.getElementById('loginBtn').addEventListener('click', function() {
             this.classList.add('loading');
             this.textContent = 'Signing in...';
